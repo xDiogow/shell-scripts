1b95bd649c2ae181ad3fe5e8feefbceafb8b7ed3
Diogo <diogoanonimos@gmail.com>
2024-09-29 18:32:50 +0200

    Added explanations/Improved UX

--- mullvad_firefox_automation.sh
+++ mullvad_firefox_automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Checks if mullvad is enabled.
+# Checks if mullvad is connected to a VPN.
 check_mullvad_status() {
     local status=$(mullvad status)
 
@@ -11,6 +11,7 @@ check_mullvad_status() {
     fi
 }
 
+# Checks if Firefox browser is running
 check_firefox() {
     result=$(ps -e | grep -i firefox)
     if [[ "$result" == *'firefox'* ]]; then
@@ -29,17 +30,17 @@ mullvad_status=$?
 if [ $mullvad_status -eq 0 ] && [ $firefox_status -eq 1 ]; then
     unset $mullvad_status
     
-    echo "Mullvad is disconnected and Firefox is running. Running mullvad!"
+    echo "Mullvad is disconnected and Firefox is running. Connecting to Mullvad!"
     mullvad connect
     
     check_mullvad_status
     mullvad_status=$?
 
     if [ $mullvad_status -eq 1 ]; then
-        echo "Connected to mullvad. You are now protected."
+        echo "Connected to mullvad! You are now protected!"
     else
-        echo "Failed to connect to mullvad."
+        echo "Failed to connect to mullvad! You are un-safe!"
     fi
 else
-    echo "Mullvad is already running or Firefox is not being used."
+    echo "Firefox is not being used or Mullvad is already protecting you."
 fi
